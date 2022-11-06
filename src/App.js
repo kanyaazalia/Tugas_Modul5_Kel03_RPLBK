bde76374095ddd25172c53cebb00578122eb6810
Kanya Azalia Andriyani <kanyaazalia@gmail.com>
2022-11-06 09:00:26 +0700

    Baru GET ke console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
+const BASE_API_URL = `https://jsonplaceholder.typicode.com`;
+
 function App() {
+  useEffect(() => {
+    async function getPosts() {
+      await axios
+        .get(`${BASE_API_URL}/posts`)
+        .then((res) => {
+          const responseData = res.data;
+          console.log(responseData);
+        })
+        .catch((error) => {
+          console.log(error);
+          window.alert(error);
+        })
+    }
+
+    getPosts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
