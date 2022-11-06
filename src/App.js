09c1bd9af6eb1c609243945e511758888c51e01e
Ade Ilham Mustofa <33013619+Zomboza@users.noreply.github.com>
2022-11-06 18:02:37 +0700

    Method POST
    bau

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { List, Paper, Typography, useStepContext } from "@mui/material";
+import { List, Paper, Typography, IconButton } from "@mui/material";
 import Card from "./components/Card.js";
+
+import { AddCircle } from "@mui/icons-material";
+import AddUserDialog from "./components/AddUserDialog";
 
 const BASE_API_URL = `https://jsonplaceholder.typicode.com`;
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [newUsers, setNewUsers] = useState([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   useEffect(() => {
     async function getPosts() {
@@ -30,6 +35,14 @@ function App() {
 
     getPosts();
   }, []);
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
 
   const handleDeletePost = (id, idx) => {
     async function delPost() {
@@ -56,6 +69,9 @@ function App() {
       <div className="list-container">
         <div className="list-title-wrapper">
           <Typography variant="h4">Posts</Typography>
+          <IconButton onClick={openDialog}>
+          <AddCircle />
+          </IconButton>
         </div>
         
         <Paper elevation={2} style={{ maxHeight: "600px", overflow: "auto" }}>
@@ -68,9 +84,25 @@ function App() {
                 onDelete={() => handleDeletePost(d.id, idx)}
               />
             ))}
+            {newUsers.map((d, idx) => (
+              <Card
+              key={d.id}
+              title={d.title}
+              body={d.body}
+              onDelete={() => handleDeletePost(d.id, idx)}
+              />
+            ))}
           </List>
         </Paper>
       </div>
+      {isDialogOpen && (
+        <AddUserDialog
+          open={isDialogOpen}
+          onClose={closeDialog}
+          users={newUsers}
+          setUsers={setNewUsers}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/AddUserDialog.js
@@ -0,0 +1,64 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField
+  } from "@mui/material";
+  import axios from "axios";
+  import React, { useState } from "react";
+  
+  const BASE_API_URL = `https://jsonplaceholder.typicode.com`;
+  
+  function AddUserDialog({ open, onClose, users, setUsers }) {
+    const [title, setName] = useState("");
+    const [body, setJob] = useState("");
+  
+    const handleSubmit = () => {
+      axios
+        .post(`${BASE_API_URL}/posts`, {
+          title: title,
+          body: body
+        })
+        .then((res) => {
+          setUsers([...users, res.data]);
+          console.log(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+  
+    return (
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle>Add dummy</DialogTitle>
+        <DialogContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: ".5rem",
+            padding: "8px 20px"
+          }}
+        >
+          <TextField
+            name="title"
+            label="title"
+            value={title}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <TextField
+            name="body"
+            label="body"
+            value={body}
+            onChange={(event) => setJob(event.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Close</Button>
+          <Button onClick={handleSubmit}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+  
+  export default AddUserDialog;
+  
